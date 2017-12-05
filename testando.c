8c9950f99827a344582245f1526e90cf0ecfde7b
blackshooter987 <34146602+blackshooter987@users.noreply.github.com>
2017-12-05 11:20:57 -0200

    Add files via upload

---
+++ testando.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+#include "structs.c"
+
+#define dim 30
+#define h 10
+#define d 5
+
+
+/*strcat(nomeTxt,"dados/listaPacientesSemana")
+conversao = 48 + *week;
+strcat(conversao, ".txt");
+strcat(nomeTxt, conversao);*/
+
+/* criar função do tipo int que recebe o struct do medico e de pacientes da semana,
+cujo funcionamento deve ser comparar o nome do medico com o nome de medico no struct de paciente
+e baseado nisso retornar o id do paciente ou 0*/
+
+int pacienteMedico(agMedico *medico, FILE *dadosPacientes){
+
+  char nomeMedico[dim],lixo[1];
+  int id;
+
+  fgets(lixo, 30, dadosPacientes);
+  fscanf(dadosPacientes, "%d", &id);
+  fgets(lixo, 30, dadosPacientes);
+  fgets(lixo, 30, dadosPacientes);
+  fgets(lixo, 30, dadosPacientes);
+  fgets(nomeMedico, 30, dadosPacientes);
+
+  if(!(strcmp(nomeMedico, medico->nome))){
+    return id;
+  }
+
+  return 0;
+
+}
