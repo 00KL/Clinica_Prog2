f9c53d9e876099b05bc0ec906b4a187042757869
00KL <lucasmoraesplay@gmail.com>
2017-12-15 07:11:26 -0200

    Add files via upload

--- criaDadosPacientes.c
+++ criaDadosPacientes.c
@@ -6,7 +6,7 @@
 
 
 //Questão 4
-void especialidadesPopulares(esp *, agMedico *, int, int *);
+void especialidadesPopulares(esp *, agMedico *, int, FILE *);
 
 //Questão 3
 void medicoPopular(agMedico *, FILE *, int);
@@ -16,15 +16,15 @@ void criaDadosPacientes(agFaixaEtaria *,esp *, agMedico *, int );
 
 void iniciaVetor(esp *);
 
-void bubble(esp *, int *);
+void bubble(esp *, int );
 
-void consultasEspSemana(esp *, agMedico *, int , int *);
+void consultasEspSemana(esp *, agMedico *, int , int );
 
 
 
 void criaDadosPacientes(agFaixaEtaria *atualizacao,esp *vetorEsp, agMedico *medico, int nMedicos){
     FILE *arqDadosClinica;
-    int numEsp=0;
+    
 
     if (!(arqDadosClinica = fopen ("saida/dadosClinica.txt", "w"))){
         printf("ERRO \n");
@@ -36,15 +36,7 @@ void criaDadosPacientes(agFaixaEtaria *atualizacao,esp *vetorEsp, agMedico *medi
     medicoPopular(medico,arqDadosClinica,nMedicos);
 
     fprintf(arqDadosClinica, "Ranking   da   procura   por   especialidades   médicas:\n\n");
-    especialidadesPopulares(vetorEsp,medico,nMedicos,&numEsp);
-    for(numEsp=numEsp-1; numEsp>=0; numEsp-- ){
-      fprintf(arqDadosClinica,"%s" ,vetorEsp[numEsp].especialidade);
-      for(int p=0; p<4;p++){
-        fprintf(arqDadosClinica,"Semana %d: %d\n",p+1,vetorEsp[numEsp].nConsultas[p]);
-      }
-      fprintf(arqDadosClinica,"\n");
-      
-    }
+    especialidadesPopulares(vetorEsp,medico,nMedicos,arqDadosClinica);
 
     fprintf(arqDadosClinica, "\nEspecialidade   mais   procurada   por   faixa   etária:\n");
     exibeTodasAsFaixas(atualizacao, arqDadosClinica);
@@ -74,10 +66,10 @@ void medicoPopular(agMedico *medico, FILE *arqDadosClinica, int nMedicos){
 }
 
 
-void especialidadesPopulares(esp *vetorEsp, agMedico *medico, int nMedicos, int *numEsp){
+void especialidadesPopulares(esp *vetorEsp, agMedico *medico, int nMedicos, FILE *arqDadosClinica){
 
   char vazio[dim] = {' '};
-  int c;
+  int c,numEsp=0;
   
   for(int k=0; k<nMedicos; k++){
    
@@ -93,7 +85,7 @@ void especialidadesPopulares(esp *vetorEsp, agMedico *medico, int nMedicos, int 
             strcpy(vetorEsp[i].especialidade,medico[k].especialidade);
             vetorEsp[i].nEspecialidade=medico[k].consultas;
             //printf("%d TESTE 2\n", medico[k].consultas);
-            *numEsp+=1;
+            numEsp+=1;
             break;
           }
 
@@ -102,8 +94,14 @@ void especialidadesPopulares(esp *vetorEsp, agMedico *medico, int nMedicos, int 
 
   bubble(vetorEsp,numEsp);
   consultasEspSemana(vetorEsp,medico,nMedicos,numEsp);
-  
 
+  for(numEsp=numEsp-1; numEsp>=0; numEsp-- ){
+      fprintf(arqDadosClinica,"%s" ,vetorEsp[numEsp].especialidade);
+      for(int p=0; p<4;p++){
+        fprintf(arqDadosClinica,"Semana %d: %d\n",p+1,vetorEsp[numEsp].nConsultas[p]);
+      }
+      fprintf(arqDadosClinica,"\n"); 
+    }
 }
 
 void iniciaVetor(esp *vetorEsp){
@@ -116,12 +114,12 @@ void iniciaVetor(esp *vetorEsp){
   }
 }
 
-void bubble(esp *vetorEsp, int *numEsp){
+void bubble(esp *vetorEsp, int numEsp){
   int troca;
   char trocaC[dim];
 
-  for(int c=0; c < *numEsp; c++){
-    for(int n=0; n < *numEsp - c - 1; n++){
+  for(int c=0; c < numEsp; c++){
+    for(int n=0; n < numEsp - c - 1; n++){
       if(vetorEsp[n].nEspecialidade > vetorEsp[n+1].nEspecialidade){
         troca = vetorEsp[n].nEspecialidade;
         strcpy(trocaC,vetorEsp[n].especialidade);
@@ -134,16 +132,15 @@ void bubble(esp *vetorEsp, int *numEsp){
   }
 }
 
-void consultasEspSemana(esp *vetorEsp, agMedico *medico, int nMedicos, int *numEsp){
+void consultasEspSemana(esp *vetorEsp, agMedico *medico, int nMedicos, int numEsp){
   for(int i=0; i<nMedicos; i++){
 
-    for(int k=0; k<*numEsp; k++){
+    for(int k=0; k<numEsp; k++){
 
       if(compStr(vetorEsp[k].especialidade,medico[i].especialidade)){
         
         for(int b=0;b<4;b++){
           vetorEsp[k].nConsultas[b]+=medico[i].nConsultasSemana[b];
-          //printf("%d -> %s",medico[i].nConsultasSemana[b],medico[i].especialidade);
         }
       }
     }
